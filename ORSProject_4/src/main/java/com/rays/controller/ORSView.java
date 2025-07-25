dd71f98a9ca4caebbb088f4da13a799a63c912e1
aastiksahu <aastiksahu8@gmail.com>
2025-07-25 19:24:30 +0530

    25-07-2025

---
+++ ORSProject_4/src/main/java/com/rays/controller/ORSView.java
@@ -0,0 +1,48 @@
+package com.rays.controller;
+
+public interface ORSView {
+
+	public String APP_CONTEXT = "/ORSProject_4";
+
+	public String PAGE_FOLDER = "/jsp";
+
+	public String WELCOME_VIEW = PAGE_FOLDER + "/WelcomeView.jsp";
+	public String WELCOME_CTL = APP_CONTEXT + "/WelcomeCtl";
+
+	public String USER_REGISTRATION_VIEW = PAGE_FOLDER + "/UserRegistrationView.jsp";
+	public String USER_REGISTRATION_CTL = APP_CONTEXT + "/UserRegistrationCtl";
+
+	public String USER_VIEW = PAGE_FOLDER + "/UserView.jsp";
+	public String USER_CTL = APP_CONTEXT + "/UserCtl";
+
+	public String LOGIN_VIEW = PAGE_FOLDER + "/LoginView.jsp";
+	public String LOGIN_CTL = APP_CONTEXT + "/LoginCtl";
+
+	public String USER_LIST_VIEW = PAGE_FOLDER + "/UserListView.jsp";
+	public String USER_LIST_CTL = APP_CONTEXT + "/UserListCtl";
+
+	public String ROLE_VIEW = PAGE_FOLDER + "/RoleView.jsp";
+	public String ROLE_CTL = APP_CONTEXT + "/RoleCtl";
+
+	public String ROLE_LIST_VIEW = PAGE_FOLDER + "/RoleListView.jsp";
+	public String ROLE_LIST_CTL = APP_CONTEXT + "/RoleListCtl";
+
+	public String COLLEGE_VIEW = PAGE_FOLDER + "/CollegeView.jsp";
+	public String COLLEGE_CTL = APP_CONTEXT + "/CollegeCtl";
+
+	public String COLLEGE_LIST_VIEW = PAGE_FOLDER + "/CollegeListView.jsp";
+	public String COLLEGE_LIST_CTL = APP_CONTEXT + "/CollegeListCtl";
+
+	public String STUDENT_VIEW = PAGE_FOLDER + "/StudentView.jsp";
+	public String STUDENT_CTL = APP_CONTEXT + "/StudentCtl";
+
+	public String STUDENT_LIST_VIEW = PAGE_FOLDER + "/StudentListView.jsp";
+	public String STUDENT_LIST_CTL = APP_CONTEXT + "/StudentListCtl";
+
+	public String MARKSHEET_VIEW = PAGE_FOLDER + "/MarksheetView.jsp";
+	public String MARKSHEET_CTL = APP_CONTEXT + "/MarksheetCtl";
+
+	public String MARKSHEET_LIST_VIEW = PAGE_FOLDER + "/MarksheetListView.jsp";
+	public String MARKSHEET_LIST_CTL = APP_CONTEXT + "/MarksheetListCtl";
+
+}
